1a00751baced9d8ae758be0b1979cac4eeb362c1
sadek1999 <sadekali1530@gmail.com>
2026-05-07 22:05:09 +0600

    first class in python

---
+++ OOP/class.py
@@ -0,0 +1,13 @@
+
+class calculator:
+     brand= 'casio '
+     color= 'black'
+
+     def add(self,n1,n2):return n1+n2
+     def deduct(self,n1,n2): return n1-n2
+     def multiply(self,a,b):return a*b
+     def divided(self,a,b):return a/b
+
+my_calculator= calculator()
+
+print("multiply",my_calculator.deduct(33,4))
