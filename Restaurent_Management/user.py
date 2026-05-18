af4a9a5dff39ba70b9220fdd474dafcefb24dd35
sadek1999 <sadekali1530@gmail.com>
2026-05-18 23:24:44 +0600

    try to make a restaurant managemetn system

--- Restaurent_Management/user.py
+++ Restaurent_Management/user.py
@@ -16,11 +16,70 @@ class Employee(User):
 class Admin(User):
     def __init__(self, name, email, address, phone):
         super().__init__(name, email, address, phone) 
+        
+
+    def add_employee(self,restaurant,employee):
+        restaurant.add_employee(employee)
+       
+    def view_employee(self,restaurant):
+        restaurant.view_employee()   
+
+    def add_new_item(self, restaurant, item):
+        restaurant.menu.add_item(item) 
+
+    def remove_item(self,restaurant ,item):
+        restaurant.menu.remove_item(item)       
+
+class RestaurantManagement: 
+    def __init__(self,name):
+        self.name= name
         self.employees=[]
+        self.menu = FoodItem()
 
-    def add_employee(self,name, email, address, phone, age, designation, salary):
-        employee= Employee(name,email,address,phone,age,designation,salary)
+    def add_employee(self,employee):
         self.employees.append(employee)
-        print(f"successfully create employee: {name}")         
+
+    def view_employee(self):
+        print("---------Employees list ------")    
+        for emp in  self.employees:
+            print(emp.name,emp.email, emp.designation ,emp.salary)
+                         
+class Menu:
+    def __init__(self):
+        self.items=[]
+
+    def add_item(self,item):
+        self.items.append(item)
+
+    def find_item(self,item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return(item) 
+           
+        return None     
+     
+    def remove_item(self,item_name):
+           item = self.find_item(item_name)
+           if item:
+               self.items.remove(item)
+           else:
+              print(f"Item not available ") 
+
+    def view_items(self):
+        print("****** Menu **********")
+        print("name \tprice\tquantity")
+        for item in self.items:
+            print(f"{item.name}\t{ item.price}\t {item.quantity}")
 
 
+class FoodItem:
+    def __init__(self,name,price,quantity):
+        self.name= name 
+        self.price= price
+        self.quantity= quantity
+
+
+f1=FoodItem("pizza",30,20)
+m1=Menu()
+m1.add_item(f1);  
+m1.view_items()           
